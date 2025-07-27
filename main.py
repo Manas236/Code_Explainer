5791d82ce4580637ac1cc33e942e42c2a535ca2a
manas <manasgawde@gmail.com>
2025-07-27 19:07:35 +0530

    6th Commit

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from typing import Dict, List, Tuple, Optional
 import time
 import threading
 from concurrent.futures import ThreadPoolExecutor
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
 
 # Configure Streamlit page
 st.set_page_config(
